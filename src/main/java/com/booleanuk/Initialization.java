3c98df1a7ea48f73da7c9a65a27fbfcd6d57e6f1
gremble0 <hermanstornes@gmail.com>
2024-08-26 09:07:55 +0200

    Complete initialization example

--- src/main/java/com/booleanuk/Initialization.java
+++ src/main/java/com/booleanuk/Initialization.java
@@ -1,4 +1,33 @@
 package com.booleanuk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Initialization {
+    public static void main(String[] args) {
+        List<Integer> numbersWorse = new ArrayList<>();
+        numbersWorse.add(420);
+        numbersWorse.add(69);
+
+        List<Integer> numbersBetter = new ArrayList<>() {{
+            add(420);
+            add(69);
+        }};
+
+        List<Integer> numbersMaybeBest = new ArrayList<>() {{
+            this.add(420);
+            this.add(69);
+        }};
+
+        for (int number : numbersWorse)
+            System.out.println(number);
+        System.out.println();
+
+        for (int number : numbersBetter)
+            System.out.println(number);
+        System.out.println();
+
+        for (int number : numbersMaybeBest)
+            System.out.println(number);
+    }
 }
